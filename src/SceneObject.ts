8f71f115ca6da912ef42956bb76ee36327ac7f37
trambz <trambz@local>
2021-11-19 21:28:40 +0100

    Adding CI build

--- src/SceneObject.ts
+++ src/SceneObject.ts
@@ -1,20 +1,23 @@
 import { Mesh, Scene } from 'three';
 
-export type Dummy = {
+export type SceneObjectProps = {
     mesh: Mesh;
-    rotate: () => void;
+    rotate: (step: number) => void;
 };
 
 export function SceneObject(
     parent: Scene,
     { position = [0, 0, 0], scale = 1, rotationSpeed = 0.05 },
     { geometry, material }
-) : Dummy {
+): SceneObjectProps {
     const dummy = {
         mesh: new Mesh(geometry, material),
-        rotate: function () {
+        rotate: function (step: number) {
             this.mesh.rotation.x += rotationSpeed;
             this.mesh.rotation.y -= rotationSpeed;
+            this.mesh.scale.x += Math.cos(step / 125) / 130;
+            this.mesh.scale.y += Math.cos(step / 125) / 130;
+            this.mesh.scale.z += Math.cos(step / 125) / 130;
         },
     };
 
