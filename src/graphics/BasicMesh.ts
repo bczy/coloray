1dce0e9e8c1b8286babfa7ffbf57c5d3cb958ddc
trambz <trambz@local>
2022-01-10 21:35:00 +0100

    Adding props

--- src/graphics/BasicMesh.ts
+++ src/graphics/BasicMesh.ts
@@ -5,24 +5,33 @@ export type SceneObjectProps = {
     animate: (step: number) => void;
 };
 
+export type BasicMeshProps = {
+    color?: number;
+    position?: number[];
+    rotation?: { x: number; y: number; z: number };
+    scale?: number;
+};
+
 export class BasicMesh {
     private mesh : Mesh;
     constructor(
         private parent: Scene,
         private geometry: BufferGeometry,
-        private rotationSpeed = { x : 0, y: 0, z: 0},
-        { position = [0, 0, 0], scale = 1 },
+        private rotationSpeed = { x: 0.01, y: 0.01, z: 0.01},
+        private color = 0x00ff00,
+        private position = [0, 0, 0], 
+        private scale = 1,
     ) {
         this.mesh = new Mesh(geometry),
-
-        this.mesh.position.x = position[0];
-        this.mesh.position.y = position[1];
-        this.mesh.position.z = position[2];
-        this.mesh.scale.x = scale;
-        this.mesh.scale.y = scale;
-        this.mesh.scale.z = scale;
-        this.mesh.userData['type'] = geometry.type;
-        parent.add(this.mesh);
+        this.mesh.position.x = this.position[0];
+        this.mesh.position.y = this.position[1];
+        this.mesh.position.z = this.position[2];
+        this.mesh.scale.x = this.scale;
+        this.mesh.scale.y = this.scale;
+        this.mesh.scale.z = this.scale;
+        this.mesh.userData['type'] = this.geometry.type;
+        this.createMaterial(this.color);
+        this.parent.add(this.mesh);
     }
 
     animate(step: number) {
