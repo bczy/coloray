0832fec0928b8c1ef36f6845859c410384f74c71
trambz <trambz@local>
2022-02-09 20:56:48 +0100

    Tweaking, linting, proper init

--- src/graphics/Scene.ts
+++ src/graphics/Scene.ts
@@ -1,9 +1,9 @@
 import {
-    Scene as ThreeScene,
-    PerspectiveCamera,
-    WebGLRenderer,
-    BoxGeometry,
-    SphereGeometry,
+  Scene as ThreeScene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  SphereGeometry,
 } from 'three';
 
 import { EffectComposer } from '../postprocessing/EffectComposer';
@@ -18,85 +18,90 @@ import { SphericalShapeGroup } from './SphericalShapeGroup';
 import { onWindowResize } from './Window';
 
 export class Scene {
-    private step = 0;
-    private scene = new ThreeScene();
-    private renderer = new WebGLRenderer();
-    private sceneLayers = new Array<ShapeGroup>();
-    private camera = new PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1
+  private step = 0;
+  private scene = new ThreeScene();
+  private renderer = new WebGLRenderer();
+  private sceneLayers = new Array<ShapeGroup>();
+  private camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1
+  );
+  private composer: EffectComposer;
+
+  private animate(): void {
+    this.step++;
+    this.sceneLayers.forEach((layer: ShapeGroup) => {
+      layer.animate(this.step);
+    });
+    this.composer.render();
+    requestAnimationFrame(() => this.animate());
+  }
+
+  constructor() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    this.camera.position.z = 15;
+
+    this.composer = new EffectComposer(this.renderer);
+    this.composer.addPass(new RenderPass(this.scene, this.camera));
+    this.composer.addPass(new UnrealBloomPass({ x: 256, y: 256 }, 2));
+    this.composer.addPass(new FilmPass(0.5, 2.5, 1024, false));
+    this.composer.addPass(new GlitchPass(32));
+
+    const sphereGeom = new SphereGeometry(undefined, 6, 6);
+    const sphereLayer = new ShapeGroup(this.scene, {
+      x: -0.01,
+      y: 0,
+      z: 0.01,
+    });
+    sphereLayer.addFromJson(this.scene, '/assets/spheres.json', sphereGeom);
+
+    this.sceneLayers.push(sphereLayer);
+
+    const extraSphereLayers = new Array<SphericalShapeGroup>();
+    const boxGeom = new BoxGeometry(4, 4, 4);
+    extraSphereLayers.push(
+      new SphericalShapeGroup(
+        this.scene,
+        { x: 0.0, y: -0.01, z: 0 },
+        boxGeom,
+        7.5,
+        18
+      )
     );
-    private composer: EffectComposer;
 
-    private animate(): void {
-        this.step++;
-        this.sceneLayers.forEach((layer: ShapeGroup) => {
-            layer.animate(this.step);
-        });
-        this.composer.render();
-        requestAnimationFrame(() => this.animate());
-    }
 
-    constructor() {
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    extraSphereLayers.push(
+      new SphericalShapeGroup(
+        this.scene,
+        { x: -0.01, y: 0.0, z: 0 },
+        sphereGeom,
+        5,
+        16,
+        0.875,
+        [
+          0x643a6d, 0x954a71, 0xbd616f, 0xd8816b, 0xe7a76d, 0xe4ab69, 0xdfb066,
+          0xdab463, 0xce954f, 0xc07641, 0xaf5837, 0x9b3930,
+        ].map((i) => i / 4)
+      )
+    );
 
-        this.camera.position.z = 15;
+    extraSphereLayers.push(
+      new SphericalShapeGroup(
+        this.scene,
+        { x: 0, y: 0.0, z: 0.01 },
+        sphereGeom,
+        1,
+        8
+      )
+    );
+    this.sceneLayers.push(...extraSphereLayers, sphereLayer);
 
-        this.composer = new EffectComposer(this.renderer);
-        this.composer.addPass(new RenderPass(this.scene, this.camera));
-        this.composer.addPass(new UnrealBloomPass({ x: 256, y: 256 }, 2));
-        this.composer.addPass(new FilmPass(0.5, 2.5, 1024, false));
-        this.composer.addPass(new GlitchPass(32));
-
-        const sphereGeom = new SphereGeometry(undefined, 6, 6);
-        const boxGeom = new BoxGeometry(4, 4, 4);
-        const sphereLayer = new ShapeGroup(this.scene, {
-            x: -0.01,
-            y: 0,
-            z: 0.01,
-        });
-        sphereLayer.addFromJson(this.scene, '/assets/spheres.json', sphereGeom);
-
-        this.sceneLayers.push(sphereLayer);
-
-        const extraSphereLayers = new Array<SphericalShapeGroup>();
-        extraSphereLayers.push(
-            new SphericalShapeGroup(
-                this.scene,
-                { x: 0.0, y: -0.01, z: 0 },
-                boxGeom,
-                7.5,
-                18
-            )
-        );
-        
-        extraSphereLayers.push(
-            new SphericalShapeGroup(
-                this.scene,
-                { x: -0.01, y: 0.0, z: 0 },
-                sphereGeom,
-                5,
-                undefined,
-                [0x643a6d, 0x954a71, 0xbd616f, 0xd8816b, 0xe7a76d, 0xe4ab69, 0xdfb066, 0xdab463, 0xce954f, 0xc07641, 0xaf5837, 0x9b3930].map(i => i / 4)
-            )
-        );
-
-        extraSphereLayers.push(
-            new SphericalShapeGroup(
-                this.scene,
-                { x: 0, y: 0.0, z: 0.01 },
-                sphereGeom,
-                1,
-                8
-            )
-        );
-        this.sceneLayers.push(...extraSphereLayers);
-
-        document.body.appendChild(this.renderer.domElement);
-        window.addEventListener('resize', () =>
-            onWindowResize(this.camera, this.renderer)
-        );
-        this.animate();
-    }
+    document.body.appendChild(this.renderer.domElement);
+    window.addEventListener('resize', () =>
+      onWindowResize(this.camera, this.renderer)
+    );
+    this.animate();
+  }
 }
--- src/graphics/SphericalShapeGroup.ts
+++ src/graphics/SphericalShapeGroup.ts
@@ -3,30 +3,34 @@ import { ShapeGroup } from './ShapeGroup';
 import { WireframedShapeProps } from './WireframedShape';
 
 export class SphericalShapeGroup extends ShapeGroup {
-    constructor(
-        scene,
-        rotationSpeed = { x: 0, y: 0.1, z: 0 },
-        geometry: BufferGeometry,
-        radius: number,
-        steps = 27,
-        colorPatterns = undefined
-    ) {
-        super(scene, rotationSpeed);
-        const shapProps = new Array<WireframedShapeProps>();
-        for (let i = 0; i < steps; i++) {
-            let wireframedShapeProps: WireframedShapeProps = {};
-            const ipi2 = i * Math.PI * 2;
-            wireframedShapeProps.position = [];
-            wireframedShapeProps.position[0] = radius * Math.cos(ipi2 / steps);
-            wireframedShapeProps.position[1] = radius * Math.sin(ipi2 / steps);
-            wireframedShapeProps.position[2] = 0;
-            wireframedShapeProps.color = colorPatterns
-                ? colorPatterns[i%colorPatterns.length]
-                : 0xfff000;
-            shapProps.push(wireframedShapeProps);
-        }
-        shapProps.forEach((sceneObjectProperties) => {
-            this.initWireFrameShape(sceneObjectProperties, scene, geometry);
-        });
+  constructor(
+    scene,
+    rotationSpeed = { x: 0, y: 0.1, z: 0 },
+    geometry: BufferGeometry,
+    radius: number,
+    steps = 27,
+    initialScale = 0.5,
+    colorPatterns = undefined
+  ) {
+    super(scene, rotationSpeed);
+    const shapProps = new Array<WireframedShapeProps>();
+    for (let i = 0; i < steps; i++) {
+      const ipi2 = i * Math.PI * 2;
+      const position = [];
+      position[0] = radius * Math.cos(ipi2 / steps);
+      position[1] = radius * Math.sin(ipi2 / steps);
+      position[2] = 0;
+      let wireframedShapeProps: WireframedShapeProps = {
+        position,
+        initialScale,
+      };
+      wireframedShapeProps.color = colorPatterns
+        ? colorPatterns[i % colorPatterns.length]
+        : 0xccc000;
+      shapProps.push(wireframedShapeProps);
     }
+    shapProps.forEach((sceneObjectProperties) => {
+      this.initWireFrameShape(sceneObjectProperties, scene, geometry);
+    });
+  }
 }
