f5d19bb55b4d662e2888a35364a095e12094bf32
trambz <trambz@local>
2022-02-09 20:56:48 +0100

    Tweaking, linting, proper init

--- src/graphics/ShapeGroup.ts
+++ src/graphics/ShapeGroup.ts
@@ -2,52 +2,48 @@ import { BufferGeometry, Scene, Group, Euler } from 'three';
 import { WireframeShape, WireframedShapeProps } from './WireframedShape';
 
 export class ShapeGroup {
-    private group = new Group();
-    private wiredFramedShapes = new Array<WireframeShape>();
-    constructor(scene, private rotationSpeed = { x: 0, y: 0, z: 0 }) {
-        this.group.rotation.x = this.rotationSpeed.x;
-        this.group.rotation.y = this.rotationSpeed.y;
-        this.group.rotation.z = this.rotationSpeed.z;
-        scene.add(this.group);
-    }
+  private group = new Group();
+  private wiredFramedShapes = new Array<WireframeShape>();
+  constructor(scene, private rotationSpeed = { x: 0, y: 0, z: 0 }) {
+    this.group.rotation.x = this.rotationSpeed.x;
+    this.group.rotation.y = this.rotationSpeed.y;
+    this.group.rotation.z = this.rotationSpeed.z;
+    scene.add(this.group);
+  }
 
-    public async addFromJson(
-        scene: Scene,
-        jsonPath: string,
-        geometry: BufferGeometry
-    ): Promise<void> {
-        const rawJson = await fetch(jsonPath);
-        const { sceneObjectsProperties, rotation } = await rawJson.json();
-        if (rotation) {
-            this.group.rotation.y = rotation.y;
-            this.group.rotation.z = rotation.z;
-            this.group.rotation.x = rotation.x;
-        }
-        sceneObjectsProperties.forEach(
-            (sceneObjectProperties: WireframedShapeProps) => {
-                this.initWireFrameShape(sceneObjectProperties, scene, geometry);
-            }
-        );
+  public async addFromJson(
+    scene: Scene,
+    jsonPath: string,
+    geometry: BufferGeometry
+  ): Promise<void> {
+    const rawJson = await fetch(jsonPath);
+    const { sceneObjectsProperties, rotation } = await rawJson.json();
+    if (rotation) {
+      this.rotationSpeed.y = rotation.y;
+      this.rotationSpeed.z = rotation.z;
+      this.rotationSpeed.x = rotation.x;
     }
+    sceneObjectsProperties.forEach(
+      (sceneObjectProperties: WireframedShapeProps) => {
+        this.initWireFrameShape(sceneObjectProperties, scene, geometry);
+      }
+    );
+  }
 
-    protected initWireFrameShape(
-        wireframedShapeProps: WireframedShapeProps,
-        scene,
-        geometry
-    ) {
-        const basicMesh = new WireframeShape(
-            scene,
-            geometry,
-            wireframedShapeProps
-        );
-        this.group.add(basicMesh.getWireframedMesh());
-        this.wiredFramedShapes.push(basicMesh);
-    }
-    public animate(step: number) {
-        this.group.rotation.x += this.rotationSpeed.x;
-        this.group.rotation.y += this.rotationSpeed.y;
-        this.wiredFramedShapes.forEach((wiredFramedShape) =>
-            wiredFramedShape.animate(step)
-        );
-    }
+  protected initWireFrameShape(
+    wireframedShapeProps: WireframedShapeProps,
+    scene,
+    geometry
+  ) {
+    const basicMesh = new WireframeShape(scene, geometry, wireframedShapeProps);
+    this.group.add(basicMesh.getWireframedMesh());
+    this.wiredFramedShapes.push(basicMesh);
+  }
+  public animate(step: number) {
+    this.group.rotation.x += this.rotationSpeed.x;
+    this.group.rotation.y += this.rotationSpeed.y;
+    this.wiredFramedShapes.forEach((wiredFramedShape) =>
+      wiredFramedShape.animate(step)
+    );
+  }
 }
